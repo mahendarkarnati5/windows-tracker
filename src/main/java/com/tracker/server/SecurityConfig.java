fbd6ac9b22dea3c3b2017136f9886ae391edbf33
mahendarkarnati5 <karnatimahendar5@gmail.com>
2026-07-23 21:33:45 +0530

    added frontend url

--- src/main/java/com/tracker/server/SecurityConfig.java
+++ src/main/java/com/tracker/server/SecurityConfig.java
@@ -84,7 +84,7 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(List.of(
                 "http://localhost:5173",
-                "https://tracker-admin-two.vercel.app"));
+                "https://windows-tracker-dashboard.vercel.app"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
